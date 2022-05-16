60c35d16678b3edbabe60996acb91136cc2970a3
susha12 <87581584+susha12@users.noreply.github.com>
2022-05-16 15:55:09 +0400

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,18 @@
-// nav bar add class black
-let nav = document.querySelector("nav");
+let welcomeButton = document.querySelector(".welcome_text__button");
 
+let welcomeText = document.querySelectorAll(".welcome_text__max");
+let sectionOneWelcome = document.querySelector(".section_one__welcome");
 
+let welcomeTextBox = document.querySelector(".welcome_text__box");
+welcomeButton.onclick = ()=>{
+    welcomeTextBox.classList.toggle("active");
+    welcomeButton.classList.toggle("active");
+    sectionOneWelcome.classList.toggle("active");
+}
+
+function clickEvent (a) {
+        welcomeText[a].classList.add("active");
+}
+function clickEventOut (a) {
+        welcomeText[a].classList.remove("active");
+}
