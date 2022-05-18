560a5d97b6e49066db7b57b8ab32a448f0bcc554
susha12 <87581584+susha12@users.noreply.github.com>
2022-05-19 00:25:06 +0400

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,22 @@
-let welcomeButton = document.querySelector(".welcome_text__button");
+// nav bar add class black
+const nav = document.querySelector("nav");
 
-let welcomeText = document.querySelectorAll(".welcome_text__max");
-let sectionOneWelcome = document.querySelector(".section_one__welcome");
+// function scroll start work
+// window.onscroll = function () {
+//     if(window.scrollY > 500){
+//         nav.classList.add("nav_black");
+//     }
+//     else if(window.scrollY <= 500){
+//         nav.classList.remove("nav_black");
+//     }
+// };
+// block welcom histori opasity
+const welcomeButton = document.querySelector(".welcome_text__button");
 
-let welcomeTextBox = document.querySelector(".welcome_text__box");
+const welcomeText = document.querySelectorAll(".welcome_text__max");
+const sectionOneWelcome = document.querySelector(".section_one__welcome");
+
+const welcomeTextBox = document.querySelector(".welcome_text__box");
 welcomeButton.onclick = ()=>{
     welcomeTextBox.classList.toggle("active");
     welcomeButton.classList.toggle("active");
@@ -16,3 +29,164 @@ function clickEvent (a) {
 function clickEventOut (a) {
         welcomeText[a].classList.remove("active");
 }
+
+
+
+
+
+// section_two_factions
+const ducksImg = document.querySelectorAll(".ducks_block_one_img");
+const contentTextBox = document.querySelectorAll(".content_body_textBox_one");
+const ducksBlock = document.querySelectorAll(".ducks_block_one");
+const contentDucks = document.querySelectorAll(".content_body_ducks");
+const ducksImgBack = document.querySelector(".button_left_duck");
+const ducksImgNext = document.querySelector(".button_right_duck");
+function caruselBackNull (){
+        if(carusel == 0){
+           caruselBack = 0;     
+        }
+        else if(carusel == 1){
+                caruselBack = 1;     
+             }
+        else if(carusel == 2){
+        caruselBack = 2;     
+        }
+};
+let carusel = 0;
+let caruselBack = 0;
+ducksImgBack.onclick = ()=>{
+        if (caruselBack == 0){
+                for(let i = 0; i < 3; i++){
+                        ducksImg[i].classList.remove('active');
+                        contentTextBox[i].classList.remove('active');
+                        ducksBlock[i].classList.remove('active');
+                }
+                ducksImg[2].classList.add('active');
+                contentTextBox[2].classList.add('active');
+                ducksBlock[2].classList.add('active');
+                ducksBlock[0].style.left = 80 +"%";
+                ducksBlock[1].style.left = 0 +"%";
+                ducksBlock[0].style.zIndex = "-1";
+                ducksBlock[2].style.zIndex = "1";
+                ducksBlock[2].style.left = 40 +"%";
+                caruselBack++;
+        }
+        else if (caruselBack == 1){
+                for(let i = 0; i < 3; i++){
+                        ducksImg[i].classList.remove('active');
+                        contentTextBox[i].classList.remove('active');
+                        ducksBlock[i].classList.remove('active');
+                }
+                ducksImg[0].classList.add('active');
+                contentTextBox[0].classList.add('active');
+                ducksBlock[0].classList.add('active');
+                ducksBlock[0].style.left = 40 +"%";
+                ducksBlock[1].style.zIndex = "-1";
+                ducksBlock[0].style.zIndex = "1";
+                ducksBlock[1].style.left = 80 +"%";
+                ducksBlock[2].style.left = 0 +"%";
+                caruselBack++;
+        }
+        else if (caruselBack == 2){
+                for(let i = 0; i < 3; i++){
+                        ducksImg[i].classList.remove('active');
+                        contentTextBox[i].classList.remove('active');
+                        ducksBlock[i].classList.remove('active');
+                }
+
+                ducksImg[1].classList.add('active');
+                contentTextBox[1].classList.add('active');
+                ducksBlock[1].classList.add('active');
+                ducksBlock[2].style.zIndex = "-1";
+                ducksBlock[1].style.zIndex = "1";
+                ducksBlock[0].style.left = 0 +"%";
+                ducksBlock[1].style.left = 40 +"%";
+                ducksBlock[2].style.left = 80 +"%";
+                caruselBack = 0;
+        }
+        caruselkNull ();
+}
+
+
+
+ducksImgNext.onclick = ()=>{
+
+        if (carusel == 0){
+                for(let i = 0; i < 3; i++){
+                        ducksImg[i].classList.remove('active');
+                        contentTextBox[i].classList.remove('active');
+                        ducksBlock[i].classList.remove('active');
+                }
+                ducksImg[carusel].classList.add('active');
+                contentTextBox[carusel].classList.add('active');
+                ducksBlock[carusel].classList.add('active');
+                ducksBlock[0].style.left = 40 +"%";
+                ducksBlock[1].style.left = 78 +"%";
+                ducksBlock[2].style.zIndex = "-1";
+                ducksBlock[0].style.zIndex = "1";
+                ducksBlock[2].style.left = 0 +"%";
+                carusel++;
+        }
+        else if (carusel == 1){
+                for(let i = 0; i < 3; i++){
+                        ducksImg[i].classList.remove('active');
+                        contentTextBox[i].classList.remove('active');
+                        ducksBlock[i].classList.remove('active');
+                }
+                ducksImg[carusel+1].classList.add('active');
+                contentTextBox[carusel+1].classList.add('active');
+                ducksBlock[carusel+1].classList.add('active');
+                ducksBlock[0].style.left = 78 +"%";
+                ducksBlock[1].style.zIndex = "-1";
+                ducksBlock[2].style.zIndex = "1";
+                ducksBlock[1].style.left = 0 +"%";
+                ducksBlock[2].style.left = 40 +"%";
+                carusel++;
+        }
+        else if (carusel == 2){
+                for(let i = 0; i < 3; i++){
+                        ducksImg[i].classList.remove('active');
+                        contentTextBox[i].classList.remove('active');
+                        ducksBlock[i].classList.remove('active');
+                }
+                ducksImg[1].classList.add('active');
+                contentTextBox[1].classList.add('active');
+                ducksBlock[1].classList.add('active');
+                ducksBlock[0].style.zIndex = "-1";
+                ducksBlock[1].style.zIndex = "1";
+                ducksBlock[0].style.left = 0 +"%";
+                ducksBlock[1].style.left = 40 +"%";
+                ducksBlock[2].style.left = 78 +"%";
+                carusel = 0;
+        }
+        caruselBackNull ();
+}
+
+
+function caruselBackNull (){
+
+
+        if(carusel == 0){
+           caruselBack = 0;     
+        }
+        else if(carusel == 1){
+                caruselBack = 2;     
+             }
+        else if(carusel == 2){
+        caruselBack = 1;     
+        }
+        console.log(carusel)
+        console.log(caruselBack)
+};
+
+function caruselkNull (){
+        if(caruselBack == 0){
+           carusel = 1;     
+        }
+        else if(caruselBack == 1){
+                carusel = 2;     
+             }
+        else if(caruselBack == 2){
+        carusel = 1;     
+        }
+};
