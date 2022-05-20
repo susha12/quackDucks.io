92501344db84c97120e9fc1a821782749135b911
susha12 <87581584+susha12@users.noreply.github.com>
2022-05-20 10:27:54 +0400

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -31,6 +31,20 @@ function clickEventOut (a) {
 }
 
 
+// header display:none
+const header = document.querySelector("header");
+
+
+window.onscroll = function () { 
+        console.log(sectionOneWelcome.getBoundingClientRect());
+        if (window.outerWidth <= 880 && sectionOneWelcome.getBoundingClientRect().y <= -48){
+                header.style.display = "none";
+                
+        }
+        else{
+                header.style.display = "flex";
+        }
+    };
 
 
 
