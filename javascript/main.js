1e3f98f4965d1bf33f5420643e176d731081f897
susha12 <87581584+susha12@users.noreply.github.com>
2022-05-21 08:23:33 +0400

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -55,7 +55,7 @@ window.onscroll = function () {
     };
 function visionStart (){
         console.log(vision.getBoundingClientRect());
-        if (window.outerWidth <= 880 && vision.getBoundingClientRect().y <= 250){
+        if (window.outerWidth <= 880 && vision.getBoundingClientRect().y <= 0){
                 visionScroll.classList.add("active");
                 console.log("yes")
                 
